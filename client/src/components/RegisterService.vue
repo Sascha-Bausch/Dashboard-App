8e0b7f493d01bcbbcf3632887b6335ad106ebe4c
SVA\sbausch <sascha.bausch@sva.de>
2022-03-16 13:24:53 +0100

    neustart

---
+++ client/src/components/RegisterService.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <h2>Register</h2>
+        <input 
+            type="email"
+            name="email"
+            v-model="email"
+            placeholder="email" />
+        <br>
+        <input
+            type="password"
+            name="password"
+            v-model="password"
+            placeholder="password" />
+        <br>
+        <button @click="register">Register</button>
+    </div>
+</template>
+
+<script>
+import AuthenticationService from "@/services/AuthenticationService"
+
+export default {
+    data () {
+        return {
+            email:'',
+            password: ''
+        }        
+    },
+    methods: {
+        async register () {
+            console.log('button was clicked', this.email, this.password)
+            await AuthenticationService.register({
+                email: this.email,
+                password: this.password
+            })
+        }
+    },
+    /* watch: {
+        email (value) {
+            console.log('email has changed', value);
+        }
+    }, */
+    /* methods: {
+        register () {
+            console.log('button was clicked', this.email, this.password);
+        }
+    }, */
+    /* mounted () {
+        
+    } */
+}
+</script>
+
+<style scoped>
+input {
+    margin: 5px;
+}
+button {
+    margin: 5px;
+}
+</style>
---
+++ client/src/views/AboutView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="about">
+    <h2>Version Eisen³</h2>
+    <router-link to="/">Go back</router-link>
+  </div>
+  <div id="app">
+    <h2>{{ date }}</h2>
+    <p>
+      Here's the current time & the number of times the button is clicked:
+      {{ showTime }}
+    </p>
+    <button @click="clickButton">Click me!</button>
+    <p>It's: {{ buttonClick }}</p>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted, watch } from "vue";
+export default {
+  setup() {
+    const showTime = ref(0);
+    function clickButton() {
+      showTime.value++;
+    }
+
+    const buttonClick = ref("");
+    watch(showTime, () => {
+      buttonClick.value = new Date().toTimeString();
+    });
+
+    let date = ref("");
+    onMounted(() => {
+      date.value = new Date().toDateString();
+    });
+
+    return {
+      date,
+      showTime,
+      clickButton,
+      buttonClick,
+    };
+  },
+};
+</script>
